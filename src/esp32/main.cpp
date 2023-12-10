89fd873f58016f9b7a0fcdab3b97016ec2fa7e03
Shao Jie Hu Chen <54322143+SH02-tech@users.noreply.github.com>
2023-12-10 01:32:14 +0100

    Add simple example of LoRa usage for ESP32 (#20)

--- src/esp32/main.cpp
+++ src/esp32/main.cpp
@@ -6,22 +6,108 @@
 
 #include "audio/microphone.h"
 #include "audio/preprocess.h"
+#include "lora/lora.h"
 
 #include "dl_tool.hpp"
 #include "birdnet_model.hpp"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #define MAIN_TAG "MAIN"
 
 // Entry point for ESP32 application
-#define TEST_WAV_SIZE 32000
+#define TEST_WAV_SIZE 44800
 
 static int8_t s_audio[TEST_WAV_SIZE];
 
 static int input_exponent = -7;
 
+void task_tx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
+	while(1) {
+		TickType_t nowTick = xTaskGetTickCount();
+		int send_len = sprintf((char *)buf,"Hello World!! %"PRIu32, nowTick);
+		lora_send_packet(buf, send_len);
+		ESP_LOGI(pcTaskGetName(NULL), "%d byte packet sent...", send_len);
+		int lost = lora_packet_lost();
+		if (lost != 0) {
+			ESP_LOGW(pcTaskGetName(NULL), "%d packets lost", lost);
+		}
+		vTaskDelay(pdMS_TO_TICKS(5000));
+	} // end while
+}
+
 extern "C" void app_main()
 {
+
+    /**********
+    LORA Module
+    ************/
+
     ESP_LOGI(MAIN_TAG, "Starting application");
+	if (lora_init() == 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "Does not recognize the module");
+		while(1) {
+			vTaskDelay(1);
+		}
+	}
+
+#if CONFIG_169MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 169MHz");
+	lora_set_frequency(169e6); // 169MHz
+#elif CONFIG_433MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 433MHz");
+	lora_set_frequency(433e6); // 433MHz
+#elif CONFIG_470MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 470MHz");
+	lora_set_frequency(470e6); // 470MHz
+#elif CONFIG_866MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 866MHz");
+	lora_set_frequency(866e6); // 866MHz
+#elif CONFIG_915MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 915MHz");
+	lora_set_frequency(915e6); // 915MHz
+#elif CONFIG_OTHER
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is %dMHz", CONFIG_OTHER_FREQUENCY);
+	long frequency = CONFIG_OTHER_FREQUENCY * 1000000;
+	lora_set_frequency(frequency);
+#endif
+
+	lora_enable_crc();
+
+	int cr = 1;
+	int bw = 7;
+	int sf = 7;
+#if CONFIF_ADVANCED
+	cr = CONFIG_CODING_RATE
+	bw = CONFIG_BANDWIDTH;
+	sf = CONFIG_SF_RATE;
+#endif
+
+	lora_set_coding_rate(cr);
+	//lora_set_coding_rate(CONFIG_CODING_RATE);
+	//cr = lora_get_coding_rate();
+	ESP_LOGI(pcTaskGetName(NULL), "coding_rate=%d", cr);
+
+	lora_set_bandwidth(bw);
+	//lora_set_bandwidth(CONFIG_BANDWIDTH);
+	//int bw = lora_get_bandwidth();
+	ESP_LOGI(pcTaskGetName(NULL), "bandwidth=%d", bw);
+
+	lora_set_spreading_factor(sf);
+	//lora_set_spreading_factor(CONFIG_SF_RATE);
+	//int sf = lora_get_spreading_factor();
+	ESP_LOGI(pcTaskGetName(NULL), "spreading_factor=%d", sf);
+
+#if CONFIG_SENDER
+	xTaskCreate(&task_tx, "TX", 1024*3, NULL, 5, NULL);
+#endif
+#if CONFIG_RECEIVER
+	xTaskCreate(&task_rx, "RX", 1024*3, NULL, 5, NULL);
+#endif
 
     /**********
     MICROPHONE
@@ -40,7 +126,7 @@ extern "C" void app_main()
     MFCC
     ************/
 
-    int num_mfcc = 32;
+    int num_mfcc = 16;
 
     int8_t **mfcc_output;
     size_t num_frames = 0;
@@ -56,16 +142,6 @@ extern "C" void app_main()
     {
         ESP_LOGE(MAIN_TAG, "MFCC output is NULL");
     }
-
-    // for (int i = 0; i < mfcc_size; i++)
-    // {
-    //     ESP_LOGI(MAIN_TAG, "Frame %d", i);
-
-    //     for (int j = 0; j < 5; j++)
-    //     {
-    //         ESP_LOGI(MAIN_TAG, "MFCC %d: %f", j, mfcc_output[i][j]);
-    //     }
-    // }
 
     /**********
     DL
@@ -115,14 +191,14 @@ extern "C" void app_main()
     BIRDNET model;
     model.forward(input);
 
-    // float *probs = model.softmax.get_output().get_element_ptr();
+    float *probs = model.softmax.get_output().get_element_ptr();
 
-    // for (int i = 0; i < 4; ++i)
-    // {
-    //     ESP_LOGI(MAIN_TAG, "Prob %d: %f", i, probs[i]);
-    // }
+    for (int i = 0; i < 4; ++i)
+    {
+        ESP_LOGI(MAIN_TAG, "Prob %d: %f", i, probs[i]);
+    }
 
-    // model.softmax.get_output().free_element();
+    model.softmax.get_output().free_element();
 
     /**********
     MFCC
