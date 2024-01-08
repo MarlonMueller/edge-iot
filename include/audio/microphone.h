8e50743c0fb1b9b5a9f725266126c56cfc93c329
MarlonMueller <marlon.mueller@tum.de>
2024-01-08 02:15:41 +0100

    24 Bit I2S MEMS

--- include/audio/microphone.h
+++ include/audio/microphone.h
@@ -1,38 +1,37 @@
 /**
  * @file microphone.h
- * @brief Microphone acquire data. 
- * 
- * @details This file contains functions to record data from microphone SPH0645. 
-*/
+ * @brief Microphone acquire data.
+ *
+ * @details This file contains functions to record data from microphone SPH0645.
+ */
 
 #pragma once
 
-#include <stdint.h>
+#include <string.h>
+#include "esp_err.h"
 
-// PIN LAYOUT: 
-// The following pins are used. Remember to also connect the power supply and
-// ground pins. 5 pins are used in total. 
-
-#define WS_PIN 17
-#define DATA_PIN 7
-#define BCLK_PIN 5
+#define SD_PIN 4 //or DOUT
+#define WS_PIN 5 //or LRCL
+#define SCK_PIN 6 //or BCLK
 
 /**
- * @brief Initialize I2S transmission for microphone. 
-*/
-void init_i2s_mic();
+ * @brief Initialize I2S transmission for microphone.
+ *
+ * @param sample_rate Sample rate of the audio signal.
+ */
+void init_i2s_mic(uint32_t sample_rate);
 
 /**
  * @brief Records audio from microphone.
- * 
- * @param buffer Buffer where data shall be stored.
- * @param num_values Number of elements to be read. 
- * 
- * @pre buffer is allocated with the number of elements indicated by num_values. 
-*/
-void record_i2s_mic(int8_t *buffer, int num_values);
+ *
+ * @param buffer Buffer to store the recorded data.
+ * @param samples Number of samples to record.
+ *
+ * @pre Buffer must be allocated.
+ */
+esp_err_t read_i2s_mic(int16_t *buffer, size_t samples);
 
 /**
- * @brief Deinitialize I2S transmission for microphone. 
-*/
+ * @brief Uninstall I2S driver.
+ */
 void deinit_i2s_mic();
--- src/audio/microphone.cpp
+++ src/audio/microphone.cpp
@@ -1,116 +1,95 @@
 /**
  * @file microphone.c
  * @brief Microphone audio recording functions
- * 
+ *
  * This file contains the implementation of the audio recording functions.
-*/
+ */
+#include <string.h>
+#include <inttypes.h>
+#include "esp_log.h"
+
+#include "driver/i2s.h"
 
 #include "audio/microphone.h"
 
-#include "driver/i2s.h"
-#include "soc/i2s_reg.h"
-
-#include "esp_log.h"
-
-#define I2S_PORT I2S_NUM_0
-#define I2S_SAMPLE_RATE (16000)
-#define I2S_READ_LEN (16 * 1024)
-
-#define IGNORE_FIRST_N_VALUES 3
-
-#define SOUND_INITIALIZE_THRESHOLD 50
-
 #define TAG "MICROPHONE"
 
-void init_i2s_mic() 
-{
-    // Parameters definitions. 
-    // Constants are defined in audio/microphone.h, drivers/i2s.h 
-    // and soc/i2s_reg.h 
+#define BUFFER_SIZE 512
+i2s_chan_handle_t rx_handle;
 
-    const i2s_config_t micCfg = {
-        .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
-        .sample_rate = I2S_SAMPLE_RATE,
+void init_i2s_mic(uint32_t sample_rate)
+{
+
+    const i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+        .sample_rate = sample_rate,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
         .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+        .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 64,
+        .dma_buf_count = 4,
         .dma_buf_len = 1024,
         .use_apll = false,
         .tx_desc_auto_clear = false,
         .fixed_mclk = 0,
     };
 
-    i2s_pin_config_t micPins = {
-        .bck_io_num = BCLK_PIN,
+    ESP_ERROR_CHECK(i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL));
+
+    i2s_pin_config_t i2s_pin_config = {
+        .bck_io_num = SCK_PIN,
         .ws_io_num = WS_PIN,
-        .data_out_num = -1,
-        .data_in_num = DATA_PIN,
+        .data_out_num = I2S_PIN_NO_CHANGE,
+        .data_in_num = SD_PIN,
     };
 
-    // Setup
+    // SPH0645 (not tested)
+    // REG_SET_BIT(I2S_RX_TIMING_REG(I2S_PORT), BIT(1)); // Delay by falling edge
+    // REG_SET_BIT(I2S_RX_CONF1_REG(I2S_PORT), I2S_RX_MSB_SHIFT); // Philips mode
 
-    i2s_driver_install(I2S_PORT, &micCfg, 0, NULL);
-
-    REG_SET_BIT(I2S_RX_TIMING_REG(I2S_PORT), BIT(1)); // Delay by falling edge
-    REG_SET_BIT(I2S_RX_CONF1_REG(I2S_PORT), I2S_RX_MSB_SHIFT); // Philips mode
-
-    i2s_set_pin(I2S_PORT, &micPins);
+    ESP_ERROR_CHECK(i2s_set_pin(I2S_NUM_0, &i2s_pin_config));
 }
 
-void record_i2s_mic(int8_t *buffer, int num_values) 
+esp_err_t read_i2s_mic(int16_t *buffer, size_t samples)
 {
-    int pos = 0;
-    char *i2s_read_buff = (char *)calloc(I2S_READ_LEN, sizeof(char));
-    size_t bytes_read = 0;
 
-    // Ignore first instants
+    /*
+     * Note: I2S reads 32-bit samples, but microphone is 24-bit.
+     * We read the upper 16 bits and discard the rest (noise and padding).
+     */
 
-    for (int i=0; i<IGNORE_FIRST_N_VALUES; ++i) {
-        i2s_read(I2S_PORT, (void *)i2s_read_buff, I2S_READ_LEN, &bytes_read, portMAX_DELAY);
-    }
+    char *buf_ptr = (char *)buffer;
+    int32_t *rx_buff = (int32_t *)malloc(BUFFER_SIZE * sizeof(int32_t));
 
-    // Trigger only when there is strong enough signal
+    while (samples > 0)
+    {
+        size_t bytes_read = 0;
+        size_t chunk = (samples > BUFFER_SIZE) ? BUFFER_SIZE : samples;
 
-    ESP_LOGI(TAG, "Waiting for recording.");
+        char *rx_buff_iter = (char *)rx_buff;
+        if (i2s_read(I2S_NUM_0, rx_buff_iter, chunk * sizeof(int32_t), &bytes_read, portMAX_DELAY) == ESP_OK)
+        {
 
-    int flag = 0;
+            size_t samples_read = bytes_read / sizeof(int32_t);
 
-    while (flag != 1) {
-        i2s_read(I2S_PORT, (void *)i2s_read_buff, I2S_READ_LEN, &bytes_read, portMAX_DELAY);
+            for (int i = 0; i < samples_read; i++)
+            {
 
-        for (int i=0; i<bytes_read && pos < num_values; ++i) {
-            int8_t value = abs((int8_t) i2s_read_buff[i]);
-            
-            if (value > SOUND_INITIALIZE_THRESHOLD) {
-                flag = 1;
+                buf_ptr[0] = rx_buff_iter[2];
+                buf_ptr[1] = rx_buff_iter[3];
+
+                rx_buff_iter += sizeof(int32_t);
+                buf_ptr += sizeof(int16_t);
             }
+            samples -= samples_read;
         }
     }
 
-    // Read values
-
-    ESP_LOGI(TAG, "Recording audio");
-    
-    while (pos < num_values) {
-
-        i2s_read(I2S_PORT, (void *)i2s_read_buff, I2S_READ_LEN, &bytes_read, portMAX_DELAY);
-
-        ESP_LOGI(TAG, "Bytes read: %d", bytes_read);
-
-        for (int i=0; i<bytes_read && pos < num_values; ++i) {
-            uint8_t value = (uint8_t) i2s_read_buff[i];
-            
-            buffer[pos] = value;
-            ++pos;
-        }
-    }
-
-    free(i2s_read_buff);
+    free(rx_buff);
+    return ESP_OK;
 }
 
-void deinit_i2s_mic()
+void deinit_i2s_mic(void)
 {
-    
+    ESP_ERROR_CHECK(i2s_driver_uninstall(I2S_NUM_0));
 }
