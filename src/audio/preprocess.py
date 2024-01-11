d554ae425b16d3642744df3837bf2a9203638bbd
Shao Jie Hu Chen <54322143+SH02-tech@users.noreply.github.com>
2024-01-11 18:57:51 +0100

    Modify Python MFCC input to int16
    The expected parameter is an array of int16 (it mocks the input from microphone in ESP32). Expected to be useful to make equivalent dataset processing and microphone processing. Example has been changed accordingly.

--- src/audio/preprocess.py
+++ src/audio/preprocess.py
@@ -16,23 +16,29 @@ import ctypes # for C library usage
 # Functions
 
 def mfcc(wav: np.array):
+    '''
+    Description: Calculate MFCC of a wav array of int16.
+    Parameters: 
+        wav - wav array of int16 (typical wav values are not int16, so reescaling is needed)
+    Return: 
+        mfcc - MFCC of wav
+    '''
 
     # Calculate data from C library
 
     lib = ctypes.CDLL('build/lib.linux-x86_64-3.10/preprocess.cpython-310-x86_64-linux-gnu.so') # Change this line to the path of the C library
 
-    lib.mfcc.argtypes = [ctypes.POINTER(ctypes.c_int8), ctypes.c_size_t, ctypes.POINTER(ctypes.POINTER(ctypes.POINTER(ctypes.c_float))), ctypes.POINTER(ctypes.c_size_t)]
+    lib.mfcc.argtypes = [ctypes.POINTER(ctypes.c_int16), ctypes.c_size_t, ctypes.POINTER(ctypes.POINTER(ctypes.POINTER(ctypes.c_float))), ctypes.POINTER(ctypes.c_size_t)]
     lib.mfcc.restype = ctypes.c_int
 
-    wav = np.array(wav)
-    wav_values = np.round(wav * (1 << 7))
-    wav_values_ptr = ctypes.cast(wav_values.ctypes.data, ctypes.POINTER(ctypes.c_int8))
+    wav = np.array(wav, dtype = np.int16) # sanity check 
+    wav_values_ptr = ctypes.cast(wav.ctypes.data, ctypes.POINTER(ctypes.c_int16))
 
     output_frames = ctypes.c_size_t()
     output = ctypes.POINTER(ctypes.POINTER(ctypes.c_float))()
 
     lib.malloc_mfcc_module()
-    lib.mfcc(wav_values_ptr, len(wav_values), ctypes.byref(output), ctypes.byref(output_frames))
+    lib.mfcc(wav_values_ptr, len(wav), ctypes.byref(output), ctypes.byref(output_frames))
     lib.free_mfcc_module()
 
     num_mfcc = int(lib.get_num_mfcc())
@@ -48,11 +54,12 @@ def mfcc(wav: np.array):
     return mfcc
 
 if __name__ == "__main__":
-    output = mfcc([1 for i in range(16000)])
+    input = np.array(np.random.randn(16000) * 2 ** 15, dtype = np.int16)
+    output = mfcc(input)
 
     plt.figure(figsize=(10, 4))
 
-    librosa.display.specshow(output)
+    librosa.display.specshow(output.T)
 
     plt.colorbar()
     plt.title("MFCC")
