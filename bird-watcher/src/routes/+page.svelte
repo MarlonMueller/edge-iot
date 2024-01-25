a78ffa39a176be124228a209a14d8589a1eefba3
Ahmed <ahmedkaddah@rocketmail.com>
2024-01-25 02:02:05 +0100

    added 1 map support with buttons to center on bird and labels on markers

--- bird-watcher/src/routes/+page.svelte
+++ bird-watcher/src/routes/+page.svelte
@@ -4,9 +4,16 @@
 	import { SlideToggle } from '@skeletonlabs/skeleton';
 	import axios from 'axios';
 	import CaliforniaCondor from '$lib/California_Condor.jpg';
-  import { MapLibre,DefaultMarker } from 'svelte-maplibre';
+	import { MapLibre, DefaultMarker, Popup } from 'svelte-maplibre';
 
 	$: birds = [];
+	$: mapCenter = [11.6654128, 48.262547];
+	$: mapZoom = 10;
+
+	let map;
+	function scrollToTop() {
+		map.scrollIntoView();
+	}
 	let fetchDataFromBackend;
 	let backendLink = 'http://172.20.10.2:8080';
 	// let backendLink = 'http://localhost:8080';
@@ -48,52 +55,64 @@
 	});
 </script>
 
-<div class="container mx-auto flex justify-center">
-	<div >
+<div class="container mx-auto flex justify-center" bind:this={map}>
+	<div style="width: 90%;">
 		<h1 class="mt-3 h1 text-center">Bird Watcher</h1>
-		<div class="mt-3 flex justify-center">
+		<div class="my-3 flex justify-center">
 			<SlideToggle name="slider-label" active="bg-primary-500" bind:checked={fetchDataFromBackend}
 				>Fetch Data</SlideToggle
 			>
 		</div>
-					
+		<div class="flex">
+			<MapLibre
+				center={mapCenter}
+				zoom={mapZoom}
+				class="map"
+				standardControls
+				style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+			>
+				{#each birds as bird}
+					{#if bird.long && (bird.long <= 90) & (bird.long >= -90) && bird.lat && (bird.lat <= 90) & (bird.lat >= -90)}
+						<DefaultMarker lngLat={[bird.long, bird.lat]}>
+							<Popup offset={[0, -10]}>
+								<div class="text-lg font-bold text-black">{bird.name}</div>
+							</Popup></DefaultMarker
+						>
+					{/if}
+				{/each}
+			</MapLibre>
+			<div class="flex flex-col-reverse w-80 mx-auto">
+				{#each birds as bird}
+					<div class="mt-6 card p-4 flex items-center flex-col" transition:fade={{ duration: 300 }}>
+						<h3 class="text-center mb-3">{bird.name}</h3>
+						<img src={CaliforniaCondor} alt="bird" style="width: 300px;" />
 
-			
-		<div class="flex flex-col-reverse">
-			{#each birds as bird}
-				<div class="mt-6 card p-4 flex items-center flex-col" transition:fade={{ duration: 300 }}>
-					<h3 class="text-center mb-3">{bird.name}</h3>
-					<img src={CaliforniaCondor} alt="bird" style="width: 300px;" />
-			
-					
-						{#if bird.long && bird.long<=90 & bird.long >=-90 &&bird.lat && bird.lat<=90 & bird.lat >=-90}
-						<h5 class="mt-3 text-center">Detected at:</h5>
-						<h5 class="text-center">{bird.long},{bird.lat}</h5>
-						<MapLibre 
-					center={[bird.long,bird.lat]}
-					zoom={12}
-					class="map"
-					standardControls
-					style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json">
-					<DefaultMarker lngLat={[bird.long,bird.lat]} >
-					</DefaultMarker>
-				</MapLibre> 
+						{#if bird.long && (bird.long <= 90) & (bird.long >= -90) && bird.lat && (bird.lat <= 90) & (bird.lat >= -90)}
+							<h5 class="mt-3 text-center">Detected at:</h5>
+							<h5 class="text-center">{bird.long},{bird.lat}</h5>
+							<button
+								type="button"
+								class="btn variant-filled mt-2"
+								on:click={() => {
+									scrollToTop();
+									mapCenter = [bird.long, bird.lat];
+									mapZoom = 13;
+								}}>Find on map</button
+							>
 						{/if}
-							
-				</div>
-				
-			{/each}
+					</div>
+				{/each}
+			</div>
 		</div>
 	</div>
 </div>
 
 <style>
 	.container {
-		min-height: 100vh;
+		min-width: 100%;
 	}
 	:global(.map) {
-		
-		height: 200px;
-				width: 400px;
-			}
+		height: 70vh;
+		width: 40%;
+	}
 </style>
