06bf37cc15e05e2ff085a08b21b544c46111351d
Ahmed <ahmedkaddah@rocketmail.com>
2024-01-25 01:25:16 +0100

    added maps

--- bird-watcher/src/routes/+page.svelte
+++ bird-watcher/src/routes/+page.svelte
@@ -4,14 +4,17 @@
 	import { SlideToggle } from '@skeletonlabs/skeleton';
 	import axios from 'axios';
 	import CaliforniaCondor from '$lib/California_Condor.jpg';
+  import { MapLibre,DefaultMarker } from 'svelte-maplibre';
 
 	$: birds = [];
 	let fetchDataFromBackend;
+	let backendLink = 'http://172.20.10.2:8080';
+	// let backendLink = 'http://localhost:8080';
 	onMount(async () => {
 		const fetchData = async () => {
 			try {
 				if (fetchDataFromBackend) {
-					const response = await axios.post('http://172.20.10.2:8080/birds/get', {});
+					const response = await axios.post(`${backendLink}/birds/get`, {});
 					if (response?.data?.statusCode === '000') {
 						const data = response.data.data;
 						if (birds.length == 0) {
@@ -46,22 +49,39 @@
 </script>
 
 <div class="container mx-auto flex justify-center">
-	<div class="space-y-10">
+	<div >
 		<h1 class="mt-3 h1 text-center">Bird Watcher</h1>
-		<div class="flex justify-center">
+		<div class="mt-3 flex justify-center">
 			<SlideToggle name="slider-label" active="bg-primary-500" bind:checked={fetchDataFromBackend}
 				>Fetch Data</SlideToggle
 			>
 		</div>
+					
 
+			
 		<div class="flex flex-col-reverse">
 			{#each birds as bird}
-				<div class="mt-10 card p-4" transition:fade={{ duration: 300 }}>
+				<div class="mt-6 card p-4 flex items-center flex-col" transition:fade={{ duration: 300 }}>
 					<h3 class="text-center mb-3">{bird.name}</h3>
 					<img src={CaliforniaCondor} alt="bird" style="width: 300px;" />
-					<h5 class="mt-3 text-center">Detected at:</h5>
-					<h5 class="text-center">{bird.long},{bird.lat}</h5>
+			
+					
+						{#if bird.long && bird.long<=90 & bird.long >=-90 &&bird.lat && bird.lat<=90 & bird.lat >=-90}
+						<h5 class="mt-3 text-center">Detected at:</h5>
+						<h5 class="text-center">{bird.long},{bird.lat}</h5>
+						<MapLibre 
+					center={[bird.long,bird.lat]}
+					zoom={12}
+					class="map"
+					standardControls
+					style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json">
+					<DefaultMarker lngLat={[bird.long,bird.lat]} >
+					</DefaultMarker>
+				</MapLibre> 
+						{/if}
+							
 				</div>
+				
 			{/each}
 		</div>
 	</div>
@@ -71,4 +91,9 @@
 	.container {
 		min-height: 100vh;
 	}
+	:global(.map) {
+		
+		height: 200px;
+				width: 400px;
+			}
 </style>
