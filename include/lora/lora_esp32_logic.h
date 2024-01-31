aea099a86c601c0eff322e47950d92db2b1ae03e
Shao Jie Hu Chen <54322143+SH02-tech@users.noreply.github.com>
2024-01-31 12:51:23 +0100

    Add doc to ESP32 LoRa logic

--- include/lora/lora_esp32_logic.h
+++ include/lora/lora_esp32_logic.h
@@ -12,10 +12,46 @@ extern "C" {
 #include <stdbool.h>
 #include <stdint.h>
 
+
+/**
+ * @brief Set up the LoRa communication. 
+ * 
+ * @post LoRa module is physically set up. 
+ * 
+ * @note LoRa module is disconnected from ESP after return from Deep Sleep. 
+ * This function needs to be called each time after wake up. 
+ */
 void setup_lora_comm();
+
+/**
+ * @brief Return the state of the connection with LoRa module.
+ * 
+ * @return True if the connection is initialized, false otherwise.
+ */
 bool is_initialized_comm();
+
+/**
+ * @brief Initialize communication protocol.
+ */
 void initialize_comm();
+
+/**
+ * @brief Send the detected classification of time frame to RPi.
+ * 
+ * @param timer_value Min until next package is expected.
+ * 
+ * @pre The LoRa communication module has been initialized. 
+ * @post Detections in time frame are resetted after successful send. 
+ */
 void send_data(uint8_t *timer_value);
+
+/**
+ * @brief Activate status of a classification index. 
+ * 
+ * @param idx Index of the classification.
+ * 
+ * @note Values are preserved until next send_data() call.
+ */
 void set_activation(int idx);
 
 #ifdef __cplusplus
